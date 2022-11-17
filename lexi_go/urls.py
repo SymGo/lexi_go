34b99982da6e141c946b9431e9f87e7b2caed98b
DJOMIDO <francheutsia@outlook.com>
2022-11-17 13:44:25 +0100

    add app

--- lexi_go/urls.py
+++ lexi_go/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myLexiGoApp import views as myLexiGoApp
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', myLexiGoApp.home),
 ]
