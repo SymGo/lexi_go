85cac11ef83b428ee85b53b615ab6fecf581fc5d
djomido <francheutsia@outlook.com>
2023-01-13 17:13:58 +0100

    combine major changes of two previous branches

--- myLexiGoApp/views.py
+++ myLexiGoApp/views.py
@@ -6,9 +6,16 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from myLexiGoApp.forms import LoginForm, RegisterForm
+from transformers import AutoModel, AutoTokenizer
+import itertools
+import torch
+import os
 
 
-nlp = spacy.load("fr_core_news_md")
+module_dir = os.path.dirname(__file__)
+
+en_nlp = spacy.load("en_core_web_sm")
+fr_nlp = spacy.load("fr_core_news_md")
 
 auth_key = "4b95de03-300f-c18a-c81a-23527d794283:fx"
 translator = deepl.Translator(auth_key)
@@ -21,12 +28,88 @@ def home(request):
 
 
 def analyze(request):
-    colis = json.loads(request.body)
-    texte = colis['inText']
+    body_unicode = request.body.decode('utf-8')
+    colis = json.loads(body_unicode)
 
-    output = translator.translate_text(texte, target_lang="EN-GB")
+    # get the chosen language level
+    niveau = str(colis['valNiveau'])
 
-    return JsonResponse({"reponse": output.text})
+    # get the vocabulary for the chosen level
+    file_path = os.path.join(
+        module_dir, '../static/lexique/list_'+niveau+'.txt')
+
+    voc = []
+    with open(file_path, encoding="utf-8", mode="r") as f:
+        for element in f.readlines():
+            token = element.strip()
+            voc.append(token)
+
+    # define inputs of awesome-align
+    src = colis['inText']
+    tgt = str(translator.translate_text(src, target_lang="EN-GB"))
+
+    # spacy docs
+    src_doc = fr_nlp(src)
+    tgt_doc = en_nlp(tgt)
+
+    # load model
+    model = AutoModel.from_pretrained("aneuraz/awesome-align-with-co")
+    tokenizer = AutoTokenizer.from_pretrained("aneuraz/awesome-align-with-co")
+
+    # model parameters
+    align_layer = 8
+    threshold = 1e-3
+
+    # pre-processing
+    sent_src, sent_tgt = [token. text for token in src_doc], [
+        token. text for token in tgt_doc]
+    token_src, token_tgt = [tokenizer.tokenize(word) for word in sent_src], [
+        tokenizer.tokenize(word) for word in sent_tgt]
+    wid_src, wid_tgt = [tokenizer.convert_tokens_to_ids(x) for x in token_src], [tokenizer.convert_tokens_to_ids(x) for x in
+                                                                                 token_tgt]
+    ids_src, ids_tgt = tokenizer.prepare_for_model(list(itertools.chain(*wid_src)), return_tensors='pt',
+                                                   model_max_length=tokenizer.model_max_length, truncation=True)[
+        'input_ids'], \
+        tokenizer.prepare_for_model(list(itertools.chain(*wid_tgt)), return_tensors='pt', truncation=True,
+                                    model_max_length=tokenizer.model_max_length)['input_ids']
+    sub2word_map_src = []
+    for i, word_list in enumerate(token_src):
+        sub2word_map_src += [i for x in word_list]
+    sub2word_map_tgt = []
+    for i, word_list in enumerate(token_tgt):
+        sub2word_map_tgt += [i for x in word_list]
+
+    # alignment
+    with torch.no_grad():
+        out_src = model(ids_src.unsqueeze(0), output_hidden_states=True)[
+            2][align_layer][0, 1:-1]
+        out_tgt = model(ids_tgt.unsqueeze(0), output_hidden_states=True)[
+            2][align_layer][0, 1:-1]
+
+        dot_prod = torch.matmul(out_src, out_tgt.transpose(-1, -2))
+
+        softmax_srctgt = torch.nn.Softmax(dim=-1)(dot_prod)
+        softmax_tgtsrc = torch.nn.Softmax(dim=-2)(dot_prod)
+
+        softmax_inter = (softmax_srctgt > threshold) * \
+            (softmax_tgtsrc > threshold)
+
+    align_subwords = torch.nonzero(softmax_inter, as_tuple=False)
+    align_words = set()
+
+    for i, j in align_subwords:
+        align_words.add((sub2word_map_src[i], sub2word_map_tgt[j]))
+
+    outText = ""
+    for i, j in sorted(align_words):
+        if sent_tgt[j] in voc:
+            outText = outText + " " + sent_tgt[j]
+        else:
+            outText = outText + " " + sent_src[i]
+
+    outText = str(outText)
+
+    return JsonResponse({"reponse": outText})
 
     # JsonResponse({"reponse": output})
 
